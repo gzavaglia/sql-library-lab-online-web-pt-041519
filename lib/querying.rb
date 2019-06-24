ecf1b77b5b64418d9567f8d6f7089c05e0f9cbe3
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-24 00:33:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,9 @@ def select_name_and_series_subgenres_of_authors
       'SELECT authors.name, subgenres.name
       FROM authors
       JOIN series
-      ON author_id = series
+      ON author_id = series.author_id
+      JOIN subgenres
+      ON series.
       '
 end
 
